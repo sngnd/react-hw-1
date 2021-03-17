42eb6407f5c1fdb69178ffc580f3748fec47c918
BaranovaAV <baranova7465@gmail.com>
2021-03-17 20:25:32 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: 0,
+    };
+  }
+
+  handleClick = (event) => {
+    const type = event.target.textContent;
+    switch (type) {
+      case "+":
+        this.setState({
+          index: ++this.state.index,
+        });
+        break;
+      case "Reset":
+        this.setState({
+          index: 0,
+        });
+        break;
+      case "-":
+        this.setState({
+          index: this.state.index === 0 ? this.state.index : --this.state.index,
+        });
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.index}</p>
+        <button onClick={this.handleClick}>+</button>
+        <button onClick={this.handleClick}>Reset</button>
+        <button onClick={(event) => this.handleClick(event)}>-</button>
+      </div>
+    );
+  }
 }
 
 export default App;
